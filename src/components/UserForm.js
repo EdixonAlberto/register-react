4e3ce20c258e29e16570a80ece5bdf676768383d
Edixon Alberto <edixonalbertto@gmail.com>
2019-09-15 06:25:23 -0400

    nuevos campos para el form
    - se crean nuevos campos para el form
    - se edita el diseño usando JSX y bootstrap
    - se capturan los eventos principales y se depuran los resultados

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class UserForm extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
-
+            id: '',
+            name: '',
+            lastname: ''
+            age: '',
+            email: '',
+            phone: '',
+            country: '',
+            created_at: ''
         }
+    }
+
+    change = e => {
+        const { name, value } = e.target;
+        console.log(name, value);
+    }
+
+    submit = e => {
+        console.log(this.state);
+        e.preventDefault();
     }
 
     render() {
@@ -14,56 +31,82 @@ export default class UserForm extends Component {
             <div className="card border-success">
 
                 {/* FORM */}
-                <form className="card-body" onSubmit={null}>
+                <form className="card-body" onSubmit={this.submit}>
                     <h5 className="text-success">Registrar Usuario</h5>
 
                     {/* NAME */}
                     <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Nombre</span>
                         <input
                             className="form-control"
                             type="text"
                             name="name"
-                            placeholder="Nombre"
-                            onChange={null}
-                            // value={this.state.name} TODO: estudiar esta forma
-                        />
+                            placeholder="Ingresa tu Nombre"
+                            onChange={this.change} />
+
                     </div>
 
-                    {/* PROFESSION */}
+                    {/* LASTNAME */}
                     <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Apellido</span>
                         <input
                             className="form-control"
                             type="text"
-                            name="profession"
-                            placeholder="Profesión"
-                            onChange={null}
-                        />
+                            name="lastname"
+                            placeholder="Ingresa tu Apellido"
+                            onChange={this.change} />
                     </div>
 
                     {/* AGE */}
                     <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Edad</span>
                         <input
                             className="form-control"
                             type="text"
                             name="age"
-                            placeholder="Edad"
-                            onChange={null}
-                        />
+                            placeholder="Ingresa tu Edad"
+                            onChange={null} />
+                    </div>
+
+                    {/* SEX */}
+                    <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Sexo</span>
+                        <select
+                            className="form-control"
+                            name="sex"
+                            onChange={null}>
+                            <option>Mujer</option>
+                            <option>Hombre</option>
+                            <option>Otro</option>
+                            <option>Prefiero no decirlo</option>
+                        </select>
                     </div>
 
                     {/* EMAIL */}
                     <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Correo Electrónico</span>
                         <input
                             className="form-control"
                             type="text"
                             name="email"
-                            placeholder="Correo"
+                            placeholder="Ingresa tu Correo Electrónico"
+                            onChange={null} />
+                    </div>
+
+                    {/* PHONE */}
+                    <div className="form-group">
+                        <span className="d-block font-weight-bold mb-1">Teléfono</span>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="phone"
+                            placeholder="Ingresa tu Teléfono"
                             onChange={null} />
                     </div>
 
                     {/* COUNTRY */}
                     <div className="form-group">
-                        <span className="d-block m-2">País</span>
+                        <span className="d-block font-weight-bold mb-1">País</span>
                         <select
                             className="form-control"
                             name="country"
@@ -78,11 +121,12 @@ export default class UserForm extends Component {
                     {/* REGISTER */}
                     <button
                         className="btn btn-primary float-right"
-                        type="submit">Registrar
+                        type="submit">
+                        <span>Registrar</span>
                     </button>
 
                 </form>
             </div>
-        )
+        );
     }
 }
